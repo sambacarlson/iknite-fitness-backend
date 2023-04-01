90dfcb5e9db1ca1181f3c9b9bb83d3818c189b5b
carlson <sambacarlson@gmail.com>
2023-04-01 16:28:51 +0100

    Created models, controllers and routes

---
+++ app.ts
@@ -0,0 +1,31 @@
+require('dotenv').config()
+import express from "express";
+import mongoose from "mongoose";
+import exerciseRouter from "./routes/exerciseRoutes";
+import categoryRouter from "./routes/categoryRoutes";
+
+
+const app = express();
+
+//connect to db and listen to changes
+const DB_URI = process.env.DB_URI;
+const PORT = process.env.PORT;
+mongoose.connect(DB_URI)
+  .then(() => {
+    app.listen(PORT);
+    //TODO: remove console.log
+    console.log(`listening on ${PORT}`);
+  })
+  .catch(error => {
+    //TODO: handle error
+    console.log(error.message);
+  });
+
+  //routes
+  app.use('/categories', categoryRouter);
+  app.use('/exercises', exerciseRouter);
+
+  // 404
+  app.use((req, res) => {
+    res.status(404).json({message: '404 not found'});
+  } );
